f1fea30ac23a510d710de988d77a519fcef328f8
Hii <hvhoangkhang@gmail.com>
2023-05-26 11:22:25 +0700

    feat001

---
+++ cmd/server/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"meraki/finalwallet/config"
+	"meraki/finalwallet/pkg/pg"
+)
+
+func main() {
+	db, err := pg.ConnectDB(config.AppConfig{
+		Host:     "172.104.41.46",
+		Port:     "5432",
+		Username: "nmadmin",
+		Password: "L9nLshJ3F7NqJgu7",
+		Dbname:   "meraki",
+	})
+	// error handling
+	if err != nil {
+		fmt.Println("Đã có lỗi xảy ra: ", err)
+		return
+	}
+	fmt.Println(db)
+}
