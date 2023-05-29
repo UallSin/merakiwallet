a763559d764fb673f532498531f481b107e92694
Hii <hvhoangkhang@gmail.com>
2023-05-29 11:29:53 +0700

    feat 02

--- config/config.go
+++ config/config.go
@@ -1,9 +1,32 @@
 package config
 
+import (
+	"github.com/caarlos0/env"
+	"github.com/joho/godotenv"
+	"log"
+)
+
+var Config AppConfig
+
 type AppConfig struct {
-	Host     string
-	Port     string
-	Username string
-	Password string
-	Dbname   string
+	Host     string `env:"DB_HOST"`
+	Port     string `env:"DB_PORT"`
+	Username string `env:"DB_USERNAME"`
+	Password string `env:"DB_PASSWORD"`
+	Dbname   string `env:"DB_NAME"`
+}
+
+func SetEnv() {
+	err := godotenv.Load("/.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	err = env.Parse(&AppConfig{})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func LoadEnv() AppConfig {
+	return Config
 }
